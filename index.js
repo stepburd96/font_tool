6e9c923ef05fe7e4ef9568647417b780c3e01f41
stepburd96 <116668596+stepburd96@users.noreply.github.com>
2023-04-20 16:04:28 -0700

    Cleaned up comments

--- index.js
+++ index.js
@@ -10,13 +10,9 @@
     const fs = require("fs");
     const os = require("os");
 
-    //Temp Report Data Structures to pass at end of process
-    //possibly setup a logs system that outputs errors to a txt file
-
     let fontStaging = [];
     let cleanedFontsLog = [];
 
-    //adds arguments to an array
     // node [entry] [arg1] [arg2] [arg3]
     // [ 'stephen', 'allen' ]
     const args = process.argv.slice(2); 
@@ -48,16 +44,9 @@
 
 function fontOpeningValidation(files) {
     //Filters out every file that isn't in the accepted file type list
-    //Return an object of arrays. Failed and Accepted Files listed by font file name as it appears in the directory
     const acceptedFileTypes = [".otf", ".ttf"];
    
     const acceptedFiles = files.filter( (file) => {
-        //Take the list of approved file types and compare it against the file. The resulting array will be the extension that was matched. 
-        //I could probably do this the other way around but this seems concise enough so I will leave it as is
-        //Possibly use .find() for a match but then I would need to split on . grab the last index and use it in .find(). Seems like the same amount of code/iteration(s) over data
-        //I prefer this method because I am comparing the extensions against the file string rather than splitting on the hope that there's an extensions
-            // I'm working off of what I have in the code rather than what will be passed in to the program
-            // Edge case: 'Helvetica' <- IDK if that can happen though
         const length = acceptedFileTypes.filter( (ext) => {
             return file.includes(ext)
         }).length;
@@ -65,8 +54,6 @@ function fontOpeningValidation(files) {
         return length > 0
     });
 
-
-    //Could probably condense both of these down into one function
     const failedFiles = files.filter( (file) => {
         const length = acceptedFileTypes.filter( (ext) => {
             return file.includes(ext)
@@ -75,7 +62,6 @@ function fontOpeningValidation(files) {
         return length == 0
     });
 
-    //return only the file names that pass the accepted file types test
     return { acceptedFiles, failedFiles } 
 }
 
